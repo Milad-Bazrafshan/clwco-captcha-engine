01a3e6df7d318d03635362c3aac757b6666fccc5
Milad <m.bazrafshan@ham-sun.com>
2025-09-21 03:47:23 -0700

    add resources and verify method to captchaService verify by get ConcurrentHashMap

--- src/main/java/org/clwco/captcha/CaptchaEngineApplication.java
+++ src/main/java/org/clwco/captcha/CaptchaEngineApplication.java
@@ -2,8 +2,10 @@ package org.clwco.captcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CaptchaEngineApplication {
 
 	public static void main(String[] args) {
